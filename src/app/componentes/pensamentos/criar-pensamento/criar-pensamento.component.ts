1d3ff8ed6a442c048877587813d75bc717c15772
Schocs <schocs13@gmail.com>
2024-01-10 18:31:46 -0300

    Inicio trabalho Angular

---
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-criar-pensamento',
+  templateUrl: './criar-pensamento.component.html',
+  styleUrls: ['./criar-pensamento.component.css']
+})
+export class CriarPensamentoComponent implements OnInit {
+  
+  ngOnInit(): void {
+    
+  }
+
+}
